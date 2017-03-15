8b5bebfb80f7f5aa38234c365802978e08620c60
Egor Prilepin <egorprilepin@mail.ru>
2017-03-15 23:48:11 +0300

    lesson 3 2/3

--- src/by/it/group473602/prilepin/lesson03/A_Huffman.java
+++ src/by/it/group473602/prilepin/lesson03/A_Huffman.java
@@ -122,20 +122,39 @@ public class A_Huffman {
         Map<Character, Integer> count = new HashMap<>();
         //1. переберем все символы по очереди и рассчитаем их частоту в Map count
         for(int i=0; i < s.length(); i++) {
-            if (count.containsKey(s.charAt(i)) == false) count.put(s.charAt(i), 0);
+            if (count.containsKey(s.charAt(i)) == false) count.put(s.charAt(i), 1);
             else count.put(s.charAt(i), count.get(s.charAt(i))+1);
         }
             //для каждого символа добавим 1 если его в карте еще нет или инкремент если есть.
 
         //2. перенесем все символы в приоритетную очередь в виде листьев
+        PriorityQueue<Node> priorityQueue = new PriorityQueue<>();
+        for (char key : count.keySet()) {
+            LeafNode leafNode = new LeafNode(count.get(key), key);
+            priorityQueue.add(leafNode);
+        }
+
         //3. вынимая по два узла из очереди (для сборки родителя)
         //и возвращая этого родителя обратно в очередь
         //построим дерево кодирования Хаффмана.
         //У родителя частоты детей складываются.
+        while(true){
+            InternalNode internalNode = new InternalNode(priorityQueue.poll(), priorityQueue.poll());
+            priorityQueue.add(internalNode);
+            if (priorityQueue.size()==1){
+                break;
+            }
+        }
 
         //4. последний из родителей будет корнем этого дерева
         //это будет последний и единственный элемент оставшийся в очереди priorityQueue.
         StringBuilder sb = new StringBuilder();
+
+        Node node = priorityQueue.poll();
+        node.fillCodes("");
+        for (int i=0; i<s.length(); i++){
+            sb.append(codes.get(s.charAt(i)));
+        }
         //.....
 
         return sb.toString();
