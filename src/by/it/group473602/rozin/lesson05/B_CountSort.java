4284404c235a0ca4b03854197bc729ca4f4da0d4
Dante <danikr@yandex.ru>
2017-03-19 21:51:28 +0300

    B_Count done

--- src/by/it/group473602/rozin/lesson05/B_CountSort.java
+++ src/by/it/group473602/rozin/lesson05/B_CountSort.java
@@ -3,6 +3,8 @@ package by.it.group473602.rozin.lesson05;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 /*
@@ -24,18 +26,30 @@ public class B_CountSort {
         //!!!!!!!!!!!!!!!!!!!!!!!!!     НАЧАЛО ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
         //размер массива
         int n = scanner.nextInt();
-        int[] points=new int[n];
+        int[] points = new int[n];
 
         //читаем точки
         for (int i = 0; i < n; i++) {
-            points[i]=scanner.nextInt();
+            points[i] = scanner.nextInt();
         }
         //тут реализуйте логику задачи с применением сортировки подсчетом
+        int max = points[0];
+        int min = points[1];
+        for (int i = 0; i < n; i++) {
+            if (points[i] < min) min = points[i];
+            if (points[i] > max) max = points[i];
+        }
+        int[] count = new int[max - min + 1];
+        for (int i = 0; i < n; i++) {
+            count[points[i] - min]++;
+        }
 
-
-
-
-
+        int index = 0;
+        for (int i = 0; i < count.length; i++) {
+            for (int j = 0; j < count[i]; j++) {
+                points[index++] = i + min;
+            }
+        }
         //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
         return points;
     }
@@ -43,11 +57,11 @@ public class B_CountSort {
 
     public static void main(String[] args) throws FileNotFoundException {
         String root = System.getProperty("user.dir") + "/src/";
-        InputStream stream = new FileInputStream(root + "by/it/a_khmelov/lesson05/dataB.txt");
+        InputStream stream = new FileInputStream(root + "by/it/group473602/rozin/lesson05/dataB.txt");
         B_CountSort instance = new B_CountSort();
-        int[] result=instance.countSort(stream);
-        for (int index:result){
-            System.out.print(index+" ");
+        int[] result = instance.countSort(stream);
+        for (int index : result) {
+            System.out.print(index + " ");
         }
     }
 
