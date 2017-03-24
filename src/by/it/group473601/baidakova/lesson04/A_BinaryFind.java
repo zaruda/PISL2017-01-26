7fb3a5c29f4df8f5abbe30b1b1298cc1df1706e4
diana.baidakova <diana.baidakova@gmail.com>
2017-03-25 01:18:18 +0300

    lesson 4 completed

--- src/by/it/group473601/baidakova/lesson04/A_BinaryFind.java
+++ src/by/it/group473601/baidakova/lesson04/A_BinaryFind.java
@@ -30,36 +30,46 @@ public class A_BinaryFind {
     int[] findIndex(InputStream stream) throws FileNotFoundException {
         //подготовка к чтению данных
         Scanner scanner = new Scanner(stream);
-        //!!!!!!!!!!!!!!!!!!!!!!!!!     НАЧАЛО ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
 
         //размер отсортированного массива
         int n = scanner.nextInt();
         //сам отсортированный массива
-        int[] a=new int[n];
+        int[] array=new int[n];
         for (int i = 1; i <= n; i++) {
-            a[i-1] = scanner.nextInt();
+            array[i-1] = scanner.nextInt();
         }
 
         //размер массива индексов
         int k = scanner.nextInt();
         int[] result=new int[k];
+
         for (int i = 0; i < k; i++) {
             int value = scanner.nextInt();
-            //тут реализуйте бинарный поиск индекса
+            result[i]=-1;
+            int left = 0;
+            int right = n-1;
 
-
-
-
-            result[i]=0;
+            while(left < right) {
+                int middle = (left+right)/2;
+                if (array[middle] == value) {
+                    result[i] = middle + 1;
+                    break;
+                }
+                else {
+                    if (array[middle] < value)
+                        left = middle + 1;
+                    else
+                        right = middle - 1;
+                }
+            }
         }
-        //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
         return result;
     }
 
 
     public static void main(String[] args) throws FileNotFoundException {
         String root = System.getProperty("user.dir") + "/src/";
-        InputStream stream = new FileInputStream(root + "by/it/a_khmelov/lesson04/dataA.txt");
+        InputStream stream = new FileInputStream(root + "by/it/a_khmelev/lesson04/dataA.txt");
         A_BinaryFind instance = new A_BinaryFind();
         //long startTime = System.currentTimeMillis();
         int[] result=instance.findIndex(stream);
