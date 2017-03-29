ca548a9a55ed15cd319af17949c03f23096b890b
Alexandr <alexkloster97@mail.ru>
2017-03-29 16:13:46 +0300

    Path to files changed

--- src/by/it/group473602/kloster/lesson06/A_LIS.java
+++ src/by/it/group473602/kloster/lesson06/A_LIS.java
@@ -55,7 +55,7 @@ public class A_LIS {
 
     public static void main(String[] args) throws FileNotFoundException {
         String root = System.getProperty("user.dir") + "/src/";
-        InputStream stream = new FileInputStream(root + "by/it/a_khmelov/lesson06/dataA.txt");
+        InputStream stream = new FileInputStream(root + "by/it/group473602/Kloster/lesson06/dataA.txt");
         A_LIS instance = new A_LIS();
         int result = instance.getSeqSize(stream);
         System.out.print(result);
