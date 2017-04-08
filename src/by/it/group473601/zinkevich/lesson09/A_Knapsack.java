645535d94a6d14a759843a5da8ee6005f4001c27
Helen <elenazinkevich05@gmail.com>
2017-04-08 19:22:30 +0300

    Lesson9 Task A complete

--- src/by/it/group473601/zinkevich/lesson09/A_Knapsack.java
+++ src/by/it/group473601/zinkevich/lesson09/A_Knapsack.java
@@ -35,28 +35,47 @@ Sample Output 2:
 
 public class A_Knapsack {
 
-    int getMaxWeight(InputStream stream ) {
+    int getMaxWeight(InputStream stream) {
         //!!!!!!!!!!!!!!!!!!!!!!!!!     НАЧАЛО ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
         Scanner scanner = new Scanner(stream);
-        int w=scanner.nextInt();
-        int n=scanner.nextInt();
-        int gold[]=new int[n];
-        for (int i = 0; i < n; i++) {
-            gold[i]=scanner.nextInt();
+        int weight = scanner.nextInt();
+        int count = scanner.nextInt();
+        int gold[] = new int[count];
+        for (int i = 0; i < count; i++) {
+            gold[i] = scanner.nextInt();
         }
 
+        int[] maxWeight = new int[weight];
+        for (int i = 0; i < weight; i++) {
+            maxWeight[i] = 0;
+        }
+        for (int i = 0; i < weight; i++) {
+            for (int j = 0; j < count; j++) {
+                if (gold[j] <= i) {
+                    maxWeight[i] = max(maxWeight[i], maxWeight[i - gold[j]] + gold[j]);
+                }
+            }
+        }
 
-        int result = 0;
+        int result = maxWeight[weight - 1];
+
         //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
         return result;
     }
 
+    int max(int number1, int number2) {
+        if (number1 > number2) {
+            return number1;
+        } else {
+            return number2;
+        }
+    }
 
     public static void main(String[] args) throws FileNotFoundException {
         String root = System.getProperty("user.dir") + "/src/";
         InputStream stream = new FileInputStream(root + "by/it/group473601/zinkevich/lesson09/dataA.txt");
         A_Knapsack instance = new A_Knapsack();
-        int res=instance.getMaxWeight(stream);
+        int res = instance.getMaxWeight(stream);
         System.out.println(res);
     }
 }
