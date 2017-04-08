c1e0ca3cfee4204c817beeea0010b0b8ab233d32
andrey <andrey_matys@mail.ru>
2017-04-08 17:51:27 +0300

    Added solution to task A,B lesson 8

---
+++ src/by/it/group473602/matys/lesson8/A_EditDist.java
@@ -0,0 +1,87 @@
+package by.it.group473602.matys.lesson8;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Scanner;
+
+/*
+Задача на программирование: расстояние Левенштейна
+    https://ru.wikipedia.org/wiki/Расстояние_Левенштейна
+    http://planetcalc.ru/1721/
+
+Дано:
+    Две данных непустые строки длины не более 100, содержащие строчные буквы латинского алфавита.
+
+Необходимо:
+    Решить задачу МЕТОДАМИ ДИНАМИЧЕСКОГО ПРОГРАММИРОВАНИЯ
+    Рекурсивно вычислить расстояние редактирования двух данных непустых строк
+
+    Sample Input 1:
+    ab
+    ab
+    Sample Output 1:
+    0
+
+    Sample Input 2:
+    short
+    ports
+    Sample Output 2:
+    3
+
+    Sample Input 3:
+    distance
+    editing
+    Sample Output 3:
+    5
+
+*/
+
+public class A_EditDist {
+
+    public static int naiveEditDistance(String sourceString, String destinationString) {
+  	int matchDist;
+  	int insertDist;
+  	int deleteDist;
+  	int swapDist;
+
+  	if (sourceString.length() == 0) {
+  	    return destinationString.length();
+  	} else if (destinationString.length() == 0) {
+  	    return sourceString.length();
+  	} else {
+  	    matchDist = naiveEditDistance(sourceString.substring(1), destinationString.substring(1));
+  	    if (sourceString.charAt(0) != destinationString.charAt(0))
+  		matchDist++;
+
+  	    insertDist = naiveEditDistance(sourceString.substring(1), destinationString) + 1;
+  	    deleteDist = naiveEditDistance(sourceString, destinationString.substring(1)) + 1;
+
+  	    if (sourceString.length() > 1 && destinationString.length() > 1
+  		    && sourceString.charAt(0) == destinationString.charAt(1)
+  		    && sourceString.charAt(1) == destinationString.charAt(0)) {
+  		swapDist = naiveEditDistance(sourceString.substring(2), destinationString.substring(2)) + 1;
+  	    } else {
+  		swapDist = Integer.MAX_VALUE;
+  	    }
+
+  	    return Math.min(matchDist, Math.min(insertDist, Math.min(deleteDist, swapDist)));
+  	}
+      }
+
+      int getDistanceEdinting(String firstString, String secondString) {
+  	return naiveEditDistance(firstString, secondString);
+      }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        String root = System.getProperty("user.dir") + "/src/";
+        InputStream stream = new FileInputStream(root + "by/it/group473602/matys/lesson8/dataABC.txt");
+        A_EditDist instance = new A_EditDist();
+        Scanner scanner = new Scanner(stream);
+        System.out.println(instance.getDistanceEdinting(scanner.nextLine(),scanner.nextLine()));
+        System.out.println(instance.getDistanceEdinting(scanner.nextLine(),scanner.nextLine()));
+        System.out.println(instance.getDistanceEdinting(scanner.nextLine(),scanner.nextLine()));
+        scanner.close();
+    }
+}
+
---
+++ src/by/it/group473602/matys/lesson8/B_EditDist.java
@@ -0,0 +1,87 @@
+package by.it.group473602.matys.lesson8;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Scanner;
+
+/*
+Задача на программирование: расстояние Левенштейна
+    https://ru.wikipedia.org/wiki/Расстояние_Левенштейна
+    http://planetcalc.ru/1721/
+
+Дано:
+    Две данных непустые строки длины не более 100, содержащие строчные буквы латинского алфавита.
+
+Необходимо:
+    Решить задачу МЕТОДАМИ ДИНАМИЧЕСКОГО ПРОГРАММИРОВАНИЯ
+    Итерационно вычислить расстояние редактирования двух данных непустых строк
+
+    Sample Input 1:
+    ab
+    ab
+    Sample Output 1:
+    0
+
+    Sample Input 2:
+    short
+    ports
+    Sample Output 2:
+    3
+
+    Sample Input 3:
+    distance
+    editing
+    Sample Output 3:
+    5
+
+*/
+
+public class B_EditDist {
+
+
+    int getDistanceEdinting(String firstString, String secondString) {
+	int m = firstString.length(), n = secondString.length();
+	int[] firstDifference;
+	int[] secondDifference = new int[n + 1];
+
+	for (int i = 0; i <= n; i++)
+	    secondDifference[i] = i;
+
+	for (int i = 1; i <= m; i++) {
+	    firstDifference = secondDifference;
+	    secondDifference = new int[n + 1];
+	    for (int j = 0; j <= n; j++) {
+		if (j == 0) {
+		    secondDifference[j] = i;
+		} else {
+		    int cost = (firstString.charAt(i - 1) != secondString.charAt(j - 1)) ? 1 : 0;
+		    if (secondDifference[j - 1] < firstDifference[j]
+			    && secondDifference[j - 1] < firstDifference[j - 1] + cost) {
+			secondDifference[j] = secondDifference[j - 1] + 1;
+		    } else if (firstDifference[j] < firstDifference[j - 1] + cost) {
+			secondDifference[j] = firstDifference[j] + 1;
+		    } else {
+			secondDifference[j] = firstDifference[j - 1] + cost;
+		    }
+		}
+	    }
+	}
+	return secondDifference[n];
+    }
+
+
+
+
+    public static void main(String[] args) throws FileNotFoundException {
+        String root = System.getProperty("user.dir") + "/src/";
+        InputStream stream = new FileInputStream(root + "by/it/group473602/matys/lesson8/dataABC.txt");
+        B_EditDist instance = new B_EditDist();
+        Scanner scanner = new Scanner(stream);
+        System.out.println(instance.getDistanceEdinting(scanner.nextLine(),scanner.nextLine()));
+        System.out.println(instance.getDistanceEdinting(scanner.nextLine(),scanner.nextLine()));
+        System.out.println(instance.getDistanceEdinting(scanner.nextLine(),scanner.nextLine()));
+        scanner.close();
+    }
+
+}
