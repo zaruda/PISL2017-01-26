4e78a25ead0d589897782a8df29605aa4d3c6906
Egor Prilepin <egorprilepin@mail.ru>
2017-04-21 22:16:51 +0300

    l7 2/3

--- src/by/it/group473602/prilepin/lesson07/B_EditDist.java
+++ src/by/it/group473602/prilepin/lesson07/B_EditDist.java
@@ -42,19 +42,51 @@ public class B_EditDist {
 
     int getDistanceEdinting(String one, String two) {
         //!!!!!!!!!!!!!!!!!!!!!!!!!     НАЧАЛО ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
-
-
-
+        int n = one.length()+1;
+        int m = two.length()+1;
+        int [][] d = new int[n][m];
+        for (int i = 0; i<n; i++){
+            d[i][0]=i;
+        }
+        for (int j = 0; j<m; j++){
+            d[0][j]=j;
+        }
         int result = 0;
+
+        for (int i = 1; i<n; i++){
+            for (int j=1; j<m; j++){
+                int c = getDiff(one.charAt(i-1), two.charAt(j-1));
+                d[i][j] = getMin(d[i-1][j]+1, d[i][j-1]+1, d[i-1][j-1]+c);
+            }
+            result = d[n-1][m-1];
+        }
         //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
         return result;
+    }
+
+    private static int getDiff(char one, char two){
+        return one != two ? 1: 0;
+    }
+
+
+    private static int getMin(int one, int two, int three){
+        int min = -1;
+        if (two > one){
+            min = one;
+        }else{
+            min = two;
+        }
+        if (three<min){
+            min = three;
+        }
+        return min;
     }
 
 
 
     public static void main(String[] args) throws FileNotFoundException {
         String root = System.getProperty("user.dir") + "/src/";
-        InputStream stream = new FileInputStream(root + "by/it/a_khmelov/lesson08/dataABC.txt");
+        InputStream stream = new FileInputStream(root + "by/it/a_khmelev/lesson08/dataABC.txt");
         B_EditDist instance = new B_EditDist();
         Scanner scanner = new Scanner(stream);
         System.out.println(instance.getDistanceEdinting(scanner.nextLine(),scanner.nextLine()));
