83af4dc4ec4af4babc4bd18910fe9237265666b7
Myrik-BY <trakhalin.oleg@gmail.com>
2017-05-17 21:29:40 +0300

    All Done

---
+++ src/by/it/group473601/trakhalin/lesson10/A_DFS.java
@@ -0,0 +1,105 @@
+package by.it.group473601.trakhalin.lesson10;
+
+//Depth-first search
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class A_DFS {
+
+    private static int counter = 1;
+
+    public static void DFS(int node, int graph[][], int n, int preVisit[], int postVisit[], int color[],
+	    int[][] edgeType) {
+
+	if (color[node] == -1) {
+	    return;
+	}
+	color[node] = 1;
+	preVisit[node] = counter++;
+	System.out.print((char) (node + 65) + ", ");
+	for (int i = 0; i < n; i++) {
+	    if (color[i] == 1 && i != node) {
+		edgeType[node][i] = -1;
+	    }
+	    if (graph[node][i] == 1 && color[i] == 0) {
+		edgeType[node][i] = 1;
+		DFS(i, graph, n, preVisit, postVisit, color, edgeType);
+	    }
+	    color[node] = -1;
+	}
+	postVisit[node] = counter++;
+    }
+
+    public static int[][] readData(InputStream stream) {
+	Scanner scanner = new Scanner(stream);
+	int numberV = scanner.nextInt();
+	int numberW = scanner.nextInt();
+
+	int[][] graph = new int[numberV][numberV];
+
+	for (int i = 0; i < numberW; i++) {
+	    int v = (int) scanner.next().charAt(0) - 65;
+	    int w = (int) scanner.next().charAt(0) - 65;
+
+	    graph[v][w] = 1;
+
+	}
+	scanner.close();
+	return graph;
+    }
+
+    public static void print(int[]preVisit, int[] postVisit, int [][]edgeType){
+	System.out.println();
+	for (int i = 0; i < preVisit.length; i++) {
+	    System.out.print(preVisit[i] + ", ");
+	}
+	System.out.println();
+	for (int i = 0; i < postVisit.length; i++) {
+	    System.out.print(postVisit[i] + ", ");
+	}
+	System.out.println();
+	for (int i = 0; i < edgeType.length; i++) {
+	    for (int j = 0; j < edgeType[i].length; j++) {
+		if (edgeType[i][j] == 1) {
+		    System.out.println("[" + i + ", " + j + "] - древесное");
+		}
+		if (edgeType[i][j] == -1) {
+		    System.out.println("[" + i + ", " + j + "] - обратное");
+		}
+	    }
+
+	}
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+	String root = System.getProperty("user.dir") + "/src/";
+	InputStream stream = new FileInputStream(root + "by/it/group473601/trakhalin/lesson10/dataA.txt");
+
+	int[][] graph = readData(stream);
+
+	// 1 - tree edge
+	// -1 - reverse edge
+	int[][] edgeType = new int[graph.length][graph.length];
+
+	int preVisit[] = new int[graph.length];
+	int postVisit[] = new int[graph.length];
+	// -1 black
+	// 0 white
+	// 1 gray
+	int color[] = new int[graph.length];
+
+	for (int i = 0; i < graph.length; i++) {
+	    if (color[i] == 0) {
+		DFS(i, graph, graph[i].length, preVisit, postVisit, color, edgeType);
+	    }
+	}
+	print(preVisit, postVisit, edgeType);
+
+
+
+    }
+}
---
+++ src/by/it/group473601/trakhalin/lesson11/Edge.java
@@ -0,0 +1,38 @@
+package by.it.group473601.trakhalin.lesson11;
+
+public class Edge {
+    int from;
+    int to;
+    int weight;
+
+    public Edge(int u, int v, int w) {
+        this.from = u;
+        this.to = v;
+        this.weight = w;
+    }
+
+    public int getFrom() {
+        return from;
+    }
+
+    public void setFrom(int from) {
+        this.from = from;
+    }
+
+    public int getTo() {
+        return to;
+    }
+
+    public void setTo(int to) {
+        this.to = to;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+}
